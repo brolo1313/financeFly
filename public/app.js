3170b50ec526df2294645b88bb58b14242cbaa81
Andrew Yupin <brolo1341@gmail.com>
2024-11-22 18:40:29 +0200

    added pwa initial source code

--- public/app.js
+++ public/app.js
@@ -97,5 +97,19 @@ function setActiveTab(activeTabId) {
   }
 }
 
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker зареєстровано з успіхом:', registration);
+      })
+      .catch((error) => {
+        console.log('Помилка реєстрації Service Worker:', error);
+      });
+  });
+}
+
 // Initialize the main view (optional, depending on your structure)
 const mainView = app.views.create(".view-main");
